71097332a869cddb36da50e2fb1c8848fa35b16a
Vinh Dang <danghuuvinh94@gmail.com>
2026-04-10 21:54:02 +1000

    feat(docker): full stack docker setup + fix frontend base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/expiry-tracker/", // 👈 PHẢI ĐÚNG TÊN REPO
+  base: "/", // 👈 PHẢI ĐÚNG TÊN REPO
 });
